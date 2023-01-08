28de6e963ed4269bdd32c92a3f0c1744dbec50c5
Adam <adam-cheaib@hotmail.com>
2023-01-08 23:41:26 +0100

    La till loop och gjorde det till en funktion

--- problem/js/functions.js
+++ problem/js/functions.js
@@ -171,43 +171,51 @@ function create_countries_cities_filters() {
 // ABSTRACT AND WRITE SPECIFICATION
 //    As you can see, all three functions below do basically the same thing.
 //    Abstract them to one function, and write the specification of that function.
+/*
+  
+    ARGUMENTS
+      The function does not take any arguments.
+  
+    SIDE EFFECTS
+      This function creates list-item elements with an id-number as attribute and 
+      text content from the LANGUAGES, LEVELS, and SUBJECTS arrays. The function 
+      then appends the list-items as children to the unordered-lists within the different 
+      containers i.e level_filter, subject_filter, and language_filter.
+  
+    NO RETURN VALUE
+  
+  */
+function create_filters() {
 
-function create_levels_filter() {
-  function create_level(level) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#level_filter > ul"),
+  for (let i = 0; i < LEVELS.length; i++) {
+    let dom_level = create_filter_element({
+      parent: document.querySelector("#level_filter ul"),
       class: "selected",
-      textContent: level.name,
+      textContent: LEVELS[i].name,
     });
-    dom.dataset.id = level.id;
-  }
-  array_each(LEVELS, create_level);
-}
+    dom_level.dataset.id = LEVELS[i].id;
+  };
 
-// Create Subjects Filter
-function create_subjects_filter() {
-  function create_subject(subject) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#subject_filter > ul"),
+
+  for (let i = 0; i < SUBJECTS.length; i++) {
+    let dom_subject = create_filter_element({
+      parent: document.querySelector("#subject_filter ul"),
       class: "selected",
-      textContent: subject.name,
+      textContent: SUBJECTS[i].name,
     });
-    dom.dataset.id = subject.id;
-  }
-  array_each(SUBJECTS, create_subject);
-}
-// Create Search Field
-function create_language_filter() {
-  function create_element(data) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#language_filter > ul"),
+    dom_subject.dataset.id = SUBJECTS[i].id;
+  };
+
+  for (let i = 0; i < LANGUAGES.length; i++) {
+    let dom_language = create_filter_element({
+      parent: document.querySelector("#language_filter ul"),
       class: "selected",
-      textContent: data.name,
+      textContent: LANGUAGES[i].name,
     });
-    dom.dataset.id = data.id;
-  }
-  array_each(LANGUAGES, create_element);
-}
+    dom_language.dataset.id = LANGUAGES[i].id;
+  };
+};
+
 
 
 // G / VG (see details in specification)
